3255db9ae89889961cf4cf64ea4dea8dfbf9be9b
uhhhh15 <menzelovagashi@gmail.com>
2025-07-30 18:07:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ import {
 
 
 // --- 插件元数据 ---
-const PLUGIN_ID = 'modern-screenshot';
-const PLUGIN_NAME = 'modern-screenshot';
-const SCRIPT_VERSION = '2.0.7';
+const PLUGIN_ID = 'html2canvas-pro';
+const PLUGIN_NAME = 'html2canvas-pro';
+const SCRIPT_VERSION = '2.1.0';
 
 // 日志系统
 const captureLogger = {
@@ -383,7 +383,7 @@ const initSettingsUI = () => {
 
         if ($(`#${OVERLAY_ID}`).length === 0) {
             const helpContentHtml = `
-                <h4>酒馆截图插件 v2.0.7 使用说明</h4>
+                <h4>酒馆截图插件 v2.1.0 使用说明</h4>
                 
                 <h5>✨ 主要更新内容</h5>
                 <h6>核心优化与修复</h6>
